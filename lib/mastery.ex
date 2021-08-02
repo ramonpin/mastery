1dda8869410acf7ece34778307c205af0dfbcf9c
ramonpin <ramon.pin@gmail.com>
2021-08-02 14:38:21 +0200

    feat: Mastery API with validations

--- lib/mastery.ex
+++ lib/mastery.ex
@@ -1,18 +1,35 @@
 defmodule Mastery do
-  @moduledoc """
-  Documentation for `Mastery`.
-  """
+  alias Mastery.Boundary.{QuizSession, QuizManager}
+  alias Mastery.Boundary.{TemplateValidator, QuizValidator}
+  alias Mastery.Core.Quiz
 
-  @doc """
-  Hello world.
+  def start_quiz_manager() do
+    QuizManager.start_link()
+  end
 
-  ## Examples
+  def build_quiz(fields) do
+    with :ok <- QuizValidator.errors(fields),
+         :ok <- QuizManager.build_quiz(fields), 
+    do: :ok, else: (error -> error)
+  end
 
-      iex> Mastery.hello()
-      :world
+  def add_template(title, fields) do
+    with :ok <- TemplateValidator.errors(fields),
+         :ok <- QuizManager.add_template(title, fields),
+    do: :ok, else: (error -> error)
+  end
 
-  """
-  def hello do
-    :world
+  def take_quiz(title, email) do
+    with %Quiz{} = quiz <- QuizManager.lookup_quiz_by_title(title),
+         {:ok, session} <- QuizSession.start_link({quiz, email}),
+    do: session, else: (error -> error)
+  end
+
+  def select_question(session) do
+    QuizSession.select_question(session)
+  end
+
+  def answer_question(session, answer) do
+    QuizSession.answer_question(session, answer)
   end
 end
